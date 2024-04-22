711aa2cd54c3dfed09b9c02495d79492e7c3110e
Gavin Thomas <gsct2002@gmail.com>
2024-04-22 17:05:41 -0600

    Added view to start working on drop down options

--- src/wiki/views/article.py
+++ src/wiki/views/article.py
@@ -1063,3 +1063,30 @@ class CreateRootView(FormView):
 class MissingRootView(TemplateView):
     template_name = "wiki/root_missing.html"
 
+class ProgressPathSearch(View):
+    @method_decorator(get_article(can_read=True))
+    def dispatch(self, request, article, *args, **kwargs):
+        max_num = kwargs.pop("max_num", 20)
+        query = request.GET.get("query", None)
+
+        matches = []
+
+        if query:
+            matches = (
+                models.URLPath.objects.can_read(request.user)
+                .active()
+                .filter(
+                    article__current_revision__title__contains=query,
+                    article__current_revision__deleted=False,
+                )
+            )
+            matches = matches.select_related_common()
+            matches = [
+                "[{title:s}](wiki:{url:s})".format(
+                    title=m.article.current_revision.title,
+                    url="/" + m.path.strip("/"),
+                )
+                for m in matches[:max_num]
+            ]
+
+        return object_to_json_response(matches)
