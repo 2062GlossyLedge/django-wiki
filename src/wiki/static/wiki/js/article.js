a93dcd908572f33590d8a3d5de2d53022b9cd3f0
aysmith17 <aysmith17@gmail.com>
2024-04-22 17:24:30 -0600

    can click line to highlight spoiler

--- src/wiki/static/wiki/js/article.js
+++ src/wiki/static/wiki/js/article.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
     $(this).addClass('list-group');
   });
 
-});
+
+
+  $('.wiki-article').find('p').on('click', function(event) {
+      if(isHighlighted){
+      $(this).css("background-color", $(this).css("background-color") === "rgb(255, 0, 0)" ? "" : "red");
+      }
+  });
+
+  
+  });
+ 
+
