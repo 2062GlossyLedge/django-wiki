7f5fa8ec79deb8703107d101575034ebe37c9e55
Nickolas Solum <solum.nickolas@gmail.com>
2024-08-25 19:57:23 -0600

    Changed logic to be server side for performance.

--- src/wiki/plugins/citations/views.py
+++ src/wiki/plugins/citations/views.py
@@ -8,11 +8,12 @@ from wiki.decorators import get_article
 class QueryUrlPath(View):
     @method_decorator(get_article(can_read=True))
     def dispatch(self, request, article, *args, **kwargs):
-        max_num = kwargs.pop("max_num", 20)
+        max_num = kwargs.pop("max_num", 100)
         query = request.GET.get("query", None)
+        wiki = request.GET.get("wiki", None)
+        subwiki = request.GET.get("subwiki", None)
 
         matches = []
-
         if query:
             matches = (
                 models.URLPath.objects.can_read(request.user)
@@ -22,6 +23,22 @@ class QueryUrlPath(View):
                     article__current_revision__deleted=False,
                 )
             )
+            
+            if subwiki:
+                subwiki = subwiki.strip('/').split('/')
+                fandom, medium, submedium = subwiki
+                matches = matches.filter(level__icontains=4)
+                matches = matches.filter(parent__slug__icontains=submedium)
+                matches = matches.filter(parent__parent__slug__icontains=medium)
+                matches = matches.filter(parent__parent__parent__slug__icontains=fandom)
+            elif wiki:
+                 # Split the wiki path into parts and strip empty strings
+                wiki = wiki.strip('/').split('/')
+                fandom, medium = wiki
+                matches = matches.filter(level__icontains=3)
+                matches = matches.filter(parent__parent__slug__icontains=fandom)
+                matches = matches.filter(parent__slug__icontains=medium)
+                
             matches = matches.select_related_common()
             matches = [
                 "{title:s} : wiki:{url:s}".format(
