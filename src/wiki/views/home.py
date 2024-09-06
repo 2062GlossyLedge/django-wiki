3042ca9e02b34a0bbcbc3cbcf8b6d277d8c398e8
aysmith17 <aysmith17@gmail.com>
2024-09-06 11:20:00 -0600

    shows profile pic on more pages

--- src/wiki/views/home.py
+++ src/wiki/views/home.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from wiki.models.account import UserProfile
+
 
 from django.shortcuts import redirect, render
 from openai import OpenAI
@@ -6,9 +9,10 @@ from openai import OpenAI
 
 class Homepage(TemplateView):
     template_name = "wiki/homepage.html"
-   
 
-
-  
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile_picture"] = UserProfile.objects.get(
+            user=self.request.user
+        ).profile_image.url
+        return context
