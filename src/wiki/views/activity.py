95a3d9856490b320a81fd408e2d546df48b591a2
Gavin Thomas <gsct2002@gmail.com>
2024-11-09 13:06:25 -0700

    created basic graph with real data for all revisions

--- src/wiki/views/activity.py
+++ src/wiki/views/activity.py
@@ -3,19 +3,29 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from datetime import timedelta
+from wiki.models.article import ArticleRevision
+from django.db.models import Count
+from django.utils import timezone
+from django.http import JsonResponse
 
 class ActivityView(TemplateView):
     template_name = "wiki/activity.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Example: Fetch activity data for the graph
-
-        # Aggregate data for the graph (customize as needed)
-        activity_data = {
-            "labels": ["Jan", "Feb", "Mar"],  # Placeholder for actual months/dates
-            "data": [10, 15, 7]  # Placeholder for activity counts
+    def get_revision_data(self):
+        # Group revisions by day and count the number of revisions per day (SQLite-compatible)
+        revisions = (
+            ArticleRevision.objects
+            .extra(select={'day': "strftime('%Y-%m-%d', created)"})
+            .values('day')
+            .annotate(revision_count=Count('id'))
+            .order_by('day')
+        )
+        return {
+            "labels": [rev["day"] for rev in revisions],
+            "data": [rev["revision_count"] for rev in revisions],
         }
 
-        context["activity_data"] = activity_data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["revision_data"] = self.get_revision_data()
         return context
